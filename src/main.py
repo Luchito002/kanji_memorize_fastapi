79edeb7def3021e92ed4fadb3904a90955929521
Luchito002 <luisfernandocalleramos4@gmail.com>
2025-11-23 23:17:49 -0400

    delete create all for database

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from src.api_response_error_handler import api_response_error_handler
-from src.database.core import engine, Base
 from src.entities import *
 from src.api import register_middlewares, register_routes
 from src.logging import configure_logging, LogLevels
@@ -8,13 +7,6 @@ from src.logging import configure_logging, LogLevels
 configure_logging(LogLevels.info)
 
 app = FastAPI()
-
-"""
-Only uncomment below to create new tables,
-otherwise the tests will fail if not connected
-"""
-
-Base.metadata.create_all(bind=engine)
 
 api_response_error_handler(app)
 register_middlewares(app)
